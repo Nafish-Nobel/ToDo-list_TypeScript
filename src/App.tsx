57766683a161b68e63c44aad3b4c1d2bd937d237
nafish nobel <nobel15-11651@diu.edu.bd>
2022-02-24 23:08:16 +0600

    all

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-pascal-case */
+import React from 'react';
+import './App.css';
+import Add_todo from './components/TodoList/Add_todo';
+
+function App() {
+  return (
+    <div className="App">
+      <Add_todo />
+    </div>
+  );
+}
+
+export default App;
